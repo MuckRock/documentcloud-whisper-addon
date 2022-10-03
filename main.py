9eba55f55a9c93c219a22aebac5bb5e3b6926c73
Mitchell Kotler <mitch@mitchellkotler.net>
2022-10-03 13:32:33 -0400

    add more config and format by segment

--- main.py
+++ main.py
@@ -7,25 +7,35 @@ import whisper
 from documentcloud.addon import AddOn
 
 
-class Whisper(AddOn):
+def format_timestamp(seconds):
+    """Convert seconds in floating point to a string timestamp"""
+    seconds = int(seconds)
+    return f"{seconds // 60:d}:{seconds % 60:02d}"
 
+
+class Whisper(AddOn):
     def main(self):
         url = self.data["url"]
+        title = self.data["title"]
+        model = self.data.get("model") or "base.en"
+        title = f"{title}.txt"
 
         with open("audio.mp3", "wb") as audio_file:
             resp = requests.get(url)
             audio_file.write(resp.content)
 
-        model = whisper.load_model("base")
+        model = whisper.load_model(model)
         result = model.transcribe("audio.mp3")
 
         print(result)
 
-        with open("transcribe.txt", "w+") as file_:
-            file_.write(result["text"])
+        with open(title, "w+") as file_:
+            for segment in results["segments"]:
+                timestamp = format_timestamp(segment["start"])
+                file_.write(f"{timestamp}: {segment['text']}\n")
             self.upload_file(file_)
-        self.client.documents.upload("transcribe.txt", original_extension="txt")
 
+        self.client.documents.upload(title, original_extension="txt")
 
 
 if __name__ == "__main__":
