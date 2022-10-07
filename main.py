1a010e2c379d909579f88dd51968c3310fb63ca0
Mitchell Kotler <mitch@mitchellkotler.net>
2022-10-07 10:10:31 -0400

    combine segments

--- main.py
+++ main.py
@@ -6,11 +6,38 @@ import requests
 import whisper
 from documentcloud.addon import AddOn
 
+MIN_WORDS = 8
 
 def format_timestamp(seconds):
     """Convert seconds in floating point to a string timestamp"""
     seconds = int(seconds)
     return f"{seconds // 60:d}:{seconds % 60:02d}"
+
+
+def format_segments(result, file):
+    """
+    Re-format Whisper segments
+
+    Whisper's segments can be overly short, so we make sure they are at least
+    complete sentences of a minimum length by combining them.
+    """
+    start = 0
+    text = ""
+    for segment in result["segments"]:
+        # if the text is at the end of a sentence and is at least MIN_WORDS
+        # words long, then write out the segment and start a new one
+        if text.endswith((".", "?", "!")) and len(text.split()) >= MIN_WORDS:
+            timestamp = format_timestamp(start)
+            file.write(f"{timestamp}: {text}\n\n")
+            start = s["start"]
+            text = s["text"]
+        else:
+            text += s["text"]
+
+    # write out the final segment
+    timestamp = format_timestamp(s["start"])
+    file.write(f"{timestamp}: {text}\n\n")
+ 
 
 
 class Whisper(AddOn):
@@ -27,12 +54,8 @@ class Whisper(AddOn):
         model = whisper.load_model(model)
         result = model.transcribe("audio.mp3")
 
-        print(result)
-
         with open(title, "w+") as file_:
-            for segment in result["segments"]:
-                timestamp = format_timestamp(segment["start"])
-                file_.write(f"{timestamp}: {segment['text']}\n\n")
+            format_segments(result, file_)
             self.upload_file(file_)
 
         self.client.documents.upload(title, original_extension="txt")
